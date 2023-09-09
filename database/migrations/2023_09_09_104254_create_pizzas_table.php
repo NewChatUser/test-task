d96dce9f4cea2c2f6af51fef6aed23fe143bdf51
newchatuser <lazarevitb@gmail.com>
2023-09-09 13:51:59 +0300

    Models reworked

---
+++ database/migrations/2023_09_09_104254_create_pizzas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->unsigned()->default(0);
+
+            $table->integer('category_id')->unsigned();
+
+            $table->string('slug')->unique();
+            $table->string('title');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id')->references('id')->on('pizza_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};
---
+++ database/migrations/2023_09_09_105039_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->unsigned()->default(0);
+
+            $table->string('slug')->unique();
+            $table->integer('basket_id');
+            $table->dateTime('order_date');
+            $table->integer('status_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('basket_id')->references('id')->on('shopping_baskets');
+            $table->foreign('status_id')->references('id')->on('order_statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
